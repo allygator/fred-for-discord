e320e5b73e68dd3bb377a79803c2123c4c21fa25
Alyssa <allygator95@gmail.com>
2020-05-14 17:22:50 -0400

    Ping works in PM and channel Fixes #1

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 client.on("message", (message) => {
-  if (message.content.substring(0, 4) === prefix) {
-    console.log("message");
-    var args = message.content.substring(4).split(" ");
+  lowerMessage = message.content.toLowerCase();
+  if (lowerMessage.substring(0, 4) === prefix) {
+    var args = lowerMessage.content.substring(4).split(" ");
     args = args.splice(1);
+    var cmd = args[0];
+    switch (cmd) {
+      // fred ping
+      case "ping":
+        message.channel.send("Pong");
+        break;
+    }
+  } else if (message.channel.type === "dm") {
+    var args = lowerMessage.substring(0).split(" ");
     var cmd = args[0];
     switch (cmd) {
       // fred ping
