d3dd7fb472bad76df21cef3bb4b5e358c94860e8
Alyssa <allygator95@gmail.com>
2020-05-24 00:53:39 -0400

    Moved the search documentation comment

--- src/commands/imageSearch.ts
+++ src/commands/imageSearch.ts
@@ -6,7 +6,6 @@ import fetch from "node-fetch";
  * Initiates a google image search on specified term and responds with a link to the image
  * @param {Discord.Messaage} message
  */
-// Search parameters at https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list
 export function imageSearch(message: Discord.Message): Promise<void> {
   var searchTerm = message.content.split(" ");
   searchTerm = searchTerm.splice(2);
@@ -14,6 +13,7 @@ export function imageSearch(message: Discord.Message): Promise<void> {
   var num = Math.floor(Math.random() * 10);
   var type =
     message.content.split(" ")[1] == "animate" ? "&imgType=animated" : "";
+  // Search parameters at https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list
   return fetch(
     "https://www.googleapis.com/customsearch/v1?key=" +
       googleToken +
