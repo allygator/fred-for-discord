dde597cdabb579ea38abe437b23439cb9c93a852
Alyssa Davis <allygator95@gmail.com>
2021-06-14 22:54:19 -0400

    Remove all the docker stuff

--- prisma/migrations/20210407031608_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Keys" (
-    "name" TEXT NOT NULL,
-    "dungeon" TEXT NOT NULL,
-    "level" INTEGER NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Keys.name_unique" ON "Keys"("name");
